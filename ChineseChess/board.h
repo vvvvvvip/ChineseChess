66c2cbf1b61b39c6ceddc6c3624e1a3758a93849
stevenshuang <stevenshuang521@gmail.com>
2018-10-13 01:00:48 +0800

    Chess Board

---
+++ ChineseChess/board.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <QWidget>
+
+class Board : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Board(QWidget *parent = nullptr);
+    virtual void paintEvent(QPaintEvent*);
+signals:
+
+public slots:
+};
+
+#endif // BOARD_H
