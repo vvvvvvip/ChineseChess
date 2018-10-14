fa2db04b3055cf493baf4ba8cbc9ffcc7d3f57f4
stevenshuang <stevenshuang521@gmail.com>
2018-10-15 01:10:15 +0800

    define get stone center func and getRect setting painter text

--- ChineseChess/board.h
+++ ChineseChess/board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 
 #include <QWidget>
+#include "stone.h"
 
 class Board : public QWidget
 {
@@ -9,6 +10,14 @@ class Board : public QWidget
 public:
     explicit Board(QWidget *parent = nullptr);
     virtual void paintEvent(QPaintEvent*);
+    void drawStone(QPainter&, int);
+    QPoint center(int, int);
+    QPoint center(int);
+    QRect getRect(int);
+
+public:
+    Stone _stone[32];
+    int d=60;
 signals:
 
 public slots:
