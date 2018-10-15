c135a3d6cb1e1fb980ce1548667823ca1fe58772
stevenshuang <stevenshuang521@gmail.com>
2018-10-16 02:06:30 +0800

    move stone

--- ChineseChess/board.h
+++ ChineseChess/board.h
@@ -10,14 +10,18 @@ class Board : public QWidget
 public:
     explicit Board(QWidget *parent = nullptr);
     virtual void paintEvent(QPaintEvent*);
+    void mouseReleaseEvent(QMouseEvent*);
+
     void drawStone(QPainter&, int);
     QPoint center(int, int);
     QPoint center(int);
     QRect getRect(int);
+    bool getRowCol(QPoint, int&, int&);
 
 public:
     Stone _stone[32];
     int d=60;
+    int _selectid;
 signals:
 
 public slots:
