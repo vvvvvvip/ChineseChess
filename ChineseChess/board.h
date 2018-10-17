afb23a76c720b064aa2f5f95416e7ddfd90af6e5
stevenshuang <stevenshuang521@gmail.com>
2018-10-18 00:28:46 +0800

    define the way of stone

--- ChineseChess/board.h
+++ ChineseChess/board.h
@@ -17,11 +17,21 @@ public:
     QPoint center(int);
     QRect getRect(int);
     bool getRowCol(QPoint, int&, int&);
-
+    bool canMove(int, int, int, int);
+    bool canMoveJiang(int, int, int, int);
+    bool canMoveShi(int, int, int, int);
+    bool canMoveXiang(int, int, int, int);
+    bool canMoveChe(int, int, int, int);
+    bool canMoveMa(int, int, int, int);
+    bool canMovePao(int, int, int, int);
+    bool canMoveBing(int, int, int, int);
+    bool judge(int, int, int, int);
+    bool existStone(int, int);   // judge (row, col) exist stone?
 public:
     Stone _stone[32];
     int d=60;
     int _selectid;
+    bool isRedWalk;
 signals:
 
 public slots:
